332d92e08a4b36b387bb41e6ff8ff21e77d56008
LY <lyan120393@troy.edu>
2018-05-30 10:17:39 -0700

    basic async test program

---
+++ node-test/utils/utils.js
@@ -0,0 +1,29 @@
+let add = (a, b) => a + b;
+let square = (a) => a * a;
+let username = (user, fullname) => {
+    //使用.split()方法,把输入的字符串根据空格分开并存入names数组的0和1的位置.
+    let names = fullname.split(' ');
+    user.firstname = names[0];
+    user.lastname = names[1];
+    return user;
+};
+
+let asyncSquare = (a, callback) => {
+    setTimeout(() => {
+        callback(a * a);
+    }, 2000)
+};
+let asyncAdd = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 2000);
+};
+
+ 
+module.exports = {
+    add,
+    square,
+    username,
+    asyncSquare,
+    asyncAdd
+};
